487192122401e57d284d19b32877ee7d66967396
gekkehenkie11 <gekkehenkie11@users.noreply.github.com>
2024-04-21 22:55:45 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,29 +1,33 @@
 #include "stdio.h"
 #include <stdint.h>
 
-void ConfigureADC()
-{
-    uint8_t* CTRLA = (uint8_t*)0x42002000;
-    uint8_t* REFCTRL = (uint8_t*)0x42002001;
-    uint8_t* SAMPCTRL = (uint8_t*)0x42002003;
-    uint16_t* CTRLB = (uint16_t*)0x42002004;
-    uint32_t* INPUTCTRL = (uint32_t*)0x42002010;
-    uint8_t* EVCTRL = (uint8_t*)0x42002014;
-    uint8_t* INTFLAG = (uint8_t*)0x42002018;
-    int8_t* STATUS = (int8_t*)0x42002019;
-    uint16_t* CALIB = (uint16_t*)0x42002028;
+int main(void) {
+typedef volatile       uint8_t  RwReg8;  /**< Read-Write  8-bit register (volatile unsigned int) */
+typedef volatile       uint16_t RwReg16; /**< Read-Write 16-bit register (volatile unsigned int) */
+typedef volatile       uint32_t RwReg;   /**< Read-Write 32-bit register (volatile unsigned int) */
+typedef volatile       uint8_t  RoReg8;  /**< Read only  8-bit register (volatile const unsigned int) */
 
-    *CTRLA = 1;
+#define REG_ADC_CTRLA              (*(RwReg8 *)0x42002000UL)
+#define REG_ADC_REFCTRL            (*(RwReg8 *)0x42002001UL)
+#define REG_ADC_SAMPCTRL           (*(RwReg8 *)0x42002003UL)
+#define REG_ADC_CALIB              (*(RwReg16*)0x42002028UL)
+#define REG_ADC_CTRLB              (*(RwReg16*)0x42002004UL)
+#define REG_ADC_INPUTCTRL          (*(RwReg  *)0x42002010UL)
+#define REG_ADC_INTFLAG            (*(RwReg8 *)0x42002018UL) /**< \brief (ADC) Interrupt Flag Status and Clear */
+#define REG_ADC_EVCTRL             (*(RwReg8 *)0x42002014UL) /**< \brief (ADC) Event Control */
+#define REG_ADC_STATUS             (*(RoReg8 *)0x42002019UL) /**< \brief (ADC) Status */
+
+
+    REG_ADC_CTRLA = 1;
   do {
-  }   while (*STATUS < 0);
+  }   while (REG_ADC_STATUS< 0);
     	 
-    *CALIB = (*((uint16_t*)0x806024) << 5) & 0x700 | *((uint16_t*)0x806020) >> 27 | (*((uint16_t*)0x806024) << 5) & 0xff | (*((uint16_t*)0x806024) & 7) << 5;
-    *SAMPCTRL = 1;
-    *REFCTRL = 3;
-    *INPUTCTRL = 0x1270000;
-    *CTRLB = 0x101;
-    *INTFLAG = 0xF;
-    *EVCTRL = 1;
+    REG_ADC_CALIB  = (*((uint16_t*)0x806024) << 5) & 0x700 | *((uint16_t*)0x806020) >> 27 | (*((uint16_t*)0x806024) << 5) & 0xff | (*((uint16_t*)0x806024) & 7) << 5;
+    REG_ADC_SAMPCTRL = 1;
+    REG_ADC_REFCTRL = 3;
+    REG_ADC_INPUTCTRL = 0x1270000;
+    REG_ADC_CTRLB = 0x101;
+    REG_ADC_INTFLAG = 0xF;
+    REG_ADC_EVCTRL = 1;
   do {
-  }  while (*STATUS < 0);
-}
+  }  while (REG_ADC_STATUS< 0);

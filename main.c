d456f9ada73288d9d329852102f1ad9b63980e9a
gekkehenkie11 <gekkehenkie11@users.noreply.github.com>
2024-04-21 22:16:23 -0400

    Update main.c

--- main.c
+++ main.c
@@ -7,12 +7,13 @@ void ConfigureADC()
     uint32_t* INPUTCTRL = (uint32_t*)0x42002010;
     uint8_t* EVCTRL = (uint8_t*)0x42002014;
     uint8_t* INTFLAG = (uint8_t*)0x42002018;
-    uint8_t* STATUS = (uint8_t*)0x42002019;
+    int8_t* STATUS = (int8_t*)0x42002019;
     uint16_t* CALIB = (uint16_t*)0x42002028;
 
     *CTRLA = 1;
-    do {
-    } while (*STATUS < 0);
+  do {
+  }   while (*STATUS < 0);
+    	 
     *CALIB = (*((uint16_t*)0x806024) << 5) & 0x700 | *((uint16_t*)0x806020) >> 27 | (*((uint16_t*)0x806024) << 5) & 0xff | (*((uint16_t*)0x806024) & 7) << 5;
     *SAMPCTRL = 1;
     *REFCTRL = 3;
@@ -20,6 +21,6 @@ void ConfigureADC()
     *CTRLB = 0x101;
     *INTFLAG = 0xF;
     *EVCTRL = 1;
-    do {
-    } while (*STATUS < 0);
+  do {
+  }  while (*STATUS < 0);
 }

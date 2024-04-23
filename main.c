dffa37789a32405f6fc8935f0d5051b42f1bf05e
gekkehenkie11 <gekkehenkie11@users.noreply.github.com>
2024-04-22 21:06:02 -0400

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 typedef volatile       uint8_t  RwReg8;  /**< Read-Write  8-bit register (volatile unsigned int) */
 typedef volatile       uint16_t RwReg16; /**< Read-Write 16-bit register (volatile unsigned int) */
 typedef volatile       uint32_t RwReg;   /**< Read-Write 32-bit register (volatile unsigned int) */
+typedef volatile       uint32_t RoReg;   /**< Read only 32-bit register (volatile const unsigned int) */
 typedef volatile       uint8_t  RoReg8;  /**< Read only  8-bit register (volatile const unsigned int) */
 
 #define REG_ADC_CTRLA              (*(RwReg8 *)0x42002000UL)
@@ -16,6 +17,57 @@ typedef volatile       uint8_t  RoReg8;  /**< Read only  8-bit register (volatil
 #define REG_ADC_EVCTRL             (*(RwReg8 *)0x42002014UL) /**< \brief (ADC) Event Control */
 #define REG_ADC_STATUS             (*(RoReg8 *)0x42002019UL) /**< \brief (ADC) Status */
 
+#define REG_SYSCTRL_PCLKSR         (*(RoReg  *)0x4000080CUL) /**< \brief (SYSCTRL) Power and Clocks Status */
+#define REG_SYSCTRL_OSC32K         (*(RwReg  *)0x40000818UL) /**< \brief (SYSCTRL) 32kHz Internal Oscillator (OSC32K) Control */
+#define REG_SYSCTRL_DFLLCTRL       (*(RwReg16*)0x40000824UL) /**< \brief (SYSCTRL) DFLL48M Control */
+#define REG_SYSCTRL_DFLLVAL        (*(RwReg  *)0x40000828UL) /**< \brief (SYSCTRL) DFLL48M Value */
+
+#define REG_GCLK_STATUS            (*(RoReg8 *)0x40000C01UL) /**< \brief (GCLK) Status */
+#define REG_GCLK_CLKCTRL           (*(RwReg16*)0x40000C02UL) /**< \brief (GCLK) Generic Clock Control */
+#define REG_GCLK_GENCTRL           (*(RwReg  *)0x40000C04UL) /**< \brief (GCLK) Generic Clock Generator Control */
+#define REG_GCLK_GENDIV            (*(RwReg  *)0x40000C08UL) /**< \brief (GCLK) Generic Clock Generator Division */
+
+#define REG_PM_APBCMASK            (*(RwReg  *)0x40000420UL) /**< \brief (PM) APBC Mask */
+
+
+void config_Sysctrl_PM_and_GCLK ()
+{
+    REG_SYSCTRL_OSC32K = 0;
+    REG_SYSCTRL_DFLLCTRL = REG_SYSCTRL_DFLLCTRL & 0xFF7F;
+    
+    do {
+    } while (((REG_SYSCTRL_PCLKSR) & 0x10) == 0); //DFLLRDY
+    
+    uint32_t calibdat = *((uint16_t*)0x806024) >> 0x1a;
+    if (calibdat == 0x3f) 
+    	calibdat = 0x1f;
+    	
+    calibdat = calibdat << 10;
+    REG_SYSCTRL_DFLLVAL = calibdat | *((uint16_t*)0x806028) & 0x3ff;
+    REG_SYSCTRL_DFLLCTRL = 2;
+    
+    do {
+    } while (((REG_SYSCTRL_PCLKSR) & 0x10) == 0); //DFLLRDY
+    
+    REG_GCLK_GENCTRL = 0x10700;
+    
+    do {
+    } while (REG_GCLK_STATUS != 0);
+    
+    REG_GCLK_GENCTRL = 0x30701;
+    REG_GCLK_GENDIV = 0x301;
+    
+    do {
+    } while (REG_GCLK_STATUS != 0);
+    
+    REG_GCLK_CLKCTRL = 0x4007;
+    REG_GCLK_CLKCTRL += 0x108;
+    REG_GCLK_CLKCTRL += 3;    
+    REG_GCLK_CLKCTRL += 1;    
+    
+    REG_PM_APBCMASK = 0x14A; 
+    REG_PM_APBCMASK = 0; 
+}
 
 void adc_config() {
 
@@ -36,5 +88,7 @@ void adc_config() {
 
 int main()
 {
+	config_Sysctrl_PM_and_GCLK ();
+	adc_config();
 	return 0 ;
 }

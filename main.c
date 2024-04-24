342439a9aa25d03592396e102e97a5528709583e
gekkehenkie11 <gekkehenkie11@users.noreply.github.com>
2024-04-23 21:56:05 -0400

    Update main.c

--- main.c
+++ main.c
@@ -31,10 +31,18 @@ typedef volatile       uint8_t  RoReg8;  /**< Read only  8-bit register (volatil
 
 #define REG_NVMCTRL_CTRLB          (*(RwReg  *)0x41004004UL) /**< \brief (NVMCTRL) Control B */
 
+#define REG_EVSYS_CHANNEL          (*(RwReg  *)0x42000404UL) /**< \brief (EVSYS) Channel */
+#define REG_EVSYS_USER             (*(RwReg16*)0x42000408UL) /**< \brief (EVSYS) User Multiplexer */
+
 void Config_NVMCTRL ()
 {
 	REG_NVMCTRL_CTRLB = 0x82;
+}
 
+void config_EventSystem ()
+{
+	REG_EVSYS_USER = 0x10C; //0001 00001100 connect channel 0, ADC_START as Event user
+	REG_EVSYS_CHANNEL = 0x61F0000; // 0000 0110 0001 1111 00000000 00000000, EVGEN: 00011111 = TC1 OVF = TC1 underflow/overflow
 }
 
 void config_Sysctrl_PM_and_GCLK ()
@@ -82,7 +90,6 @@ void adc_config() {
 	do {
 	}   while (REG_ADC_STATUS != 0);
     
-    
 	uint32_t tmp =  (*((uint32_t*)0x806024) << 5) & 0x700 | *((uint32_t*)0x806020) >> 27 | (*((uint32_t*)0x806024) << 5) & 0xff | ((*((uint32_t*)0x806024) & 7) << 5);	 
 	REG_ADC_CALIB = tmp;    
 	REG_ADC_SAMPCTRL = 1;
@@ -97,8 +104,10 @@ void adc_config() {
 
 int main()
 {
+	REG_NVMCTRL_CTRLB = 6;
 	config_Sysctrl_PM_and_GCLK ();
 	Config_NVMCTRL();
+	config_EventSystem();
 	adc_config();
 	return 0 ;
 }
